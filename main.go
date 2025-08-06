e97bb055af70cc152a097b19a70d484ee6693c81
MeikeB <meike.buysse@gmail.com>
2025-08-06 14:02:15 +0200

    Basic REPL

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		fmt.Println("Your command was:", cleanInput(input)[0])
+	}
 }
 
 // Split user input based on whitespaces.
