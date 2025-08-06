235202d4e62743cde26136e31d7abce74d984c07
MeikeB <meike.buysse@gmail.com>
2025-08-06 14:19:08 +0200

    Added commands

--- main.go
+++ main.go
@@ -7,13 +7,38 @@ import (
 	"strings"
 )
 
+var commands map[string]cliCommand
+
 func main() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		input := scanner.Text()
-		fmt.Println("Your command was:", cleanInput(input)[0])
+		input := cleanInput(scanner.Text())
+
+		command, exists := commands[input[0]]
+		if !exists {
+			fmt.Println("Unknown command")
+		} else {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
 	}
 }
 
@@ -31,4 +56,24 @@ func cleanInput(text string) []string {
 	}
 
 	return result
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for _, c := range commands {
+		fmt.Printf("\t%s: %s\n", c.name, c.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
 }
